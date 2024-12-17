e170642744ca59ce17363937aa935d730c6af033
Omega24Rofi <iamrofif@gmail.com>
2024-12-17 14:07:52 +0700

    add navbar

---
+++ app/src/main/java/com/example/ghiblipedia/Fragment/ProfileFragment.kt
@@ -0,0 +1,6 @@
+package com.example.ghiblipedia.Fragment
+
+import androidx.fragment.app.Fragment
+
+class ProfileFragment: Fragment() {
+}
--- app/src/main/java/com/example/ghiblipedia/User/HomeActivity.kt
+++ app/src/main/java/com/example/ghiblipedia/User/HomeActivity.kt
@@ -3,14 +3,19 @@ package com.example.ghiblipedia.User
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import com.example.ghiblipedia.databinding.HomeActivityBinding
+import androidx.navigation.findNavController
+import com.example.ghiblipedia.databinding.ActivityHomeBinding
 
 class HomeActivity: AppCompatActivity() {
-    private lateinit var binding: HomeActivityBinding
+    private lateinit var binding: ActivityHomeBinding
     override fun onCreate(savedInstanceState: Bundle?) {
-        binding = HomeActivityBinding.inflate(layoutInflater)
+        binding = ActivityHomeBinding.inflate(layoutInflater)
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(binding.root)
+
+        with(binding){
+            val navController = findNavController(BottomN)
+        }
     }
 }
